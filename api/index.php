7d121acd3ba0cc3d02c73414551e50243c81295c
hoaiphuongdg26 <hoaiphuong260803@gmail.com>
2024-06-05 10:32:38 +0700

    Revert "deploy"
    This reverts commit c6532028420a8ecfd20ce4d31bdd9fd3e2c5a6fe.

--- api/index.php
+++ api/index.php
@@ -4,14 +4,6 @@
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Check if the request is for the API
-$isApiRequest = stripos($_SERVER['REQUEST_URI'], '/api/') === 0;
-
-// Set the correct path for API requests
-if ($isApiRequest) {
-    $app->instance('path', base_path('routes/api.php'));
-}
-
 // Run the application
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
